3fa9d2721d5b30caf7ba6a49e9d764b1e7185d52
tunchamroeun <chamroeuntun25@outlook.com>
2019-08-21 17:45:06 +0700

    material-ui

---
+++ resources/views/dashboard/sidebar.blade.php
@@ -0,0 +1,110 @@
+<div class="sidebar sidebar-dark sidebar-main sidebar-expand-md sidebar-fixed">
+
+    <!-- Sidebar mobile toggler -->
+    <div class="sidebar-mobile-toggler text-center">
+        <a href="#" class="sidebar-mobile-main-toggle">
+            <i class="icon-arrow-left8"></i>
+        </a>
+        Navigation
+        <a href="#" class="sidebar-mobile-expand">
+            <i class="icon-screen-full"></i>
+            <i class="icon-screen-normal"></i>
+        </a>
+    </div>
+    <!-- /sidebar mobile toggler -->
+
+
+    <!-- Sidebar content -->
+    <div class="sidebar-content">
+
+        <!-- User menu -->
+        <div class="sidebar-user-material">
+            <div class="sidebar-user-material-body">
+                <div class="card-body text-center">
+                    <a href="#">
+                        <img src="{{asset('ui/global_assets/images/image.png')}}" class="img-fluid rounded-circle shadow-1 mb-3" width="80" height="80" alt="">
+                    </a>
+                    <h6 class="mb-0 text-white text-shadow-dark">Victoria Baker</h6>
+                    <span class="font-size-sm text-white text-shadow-dark">Santa Ana, CA</span>
+                </div>
+
+                <div class="sidebar-user-material-footer">
+                    <a href="#user-nav" class="d-flex justify-content-between align-items-center text-shadow-dark dropdown-toggle" data-toggle="collapse"><span>Navigation</span></a>
+                </div>
+            </div>
+
+            <div class="collapse" id="user-nav">
+                <ul class="nav nav-sidebar">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-user-plus"></i>
+                            <span>My profile</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-coins"></i>
+                            <span>My balance</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-comment-discussion"></i>
+                            <span>Messages</span>
+                            <span class="badge bg-teal-400 badge-pill align-self-center ml-auto">58</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-cog5"></i>
+                            <span>Account settings</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="icon-switch2"></i>
+                            <span>Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- /user menu -->
+
+
+        <!-- Main navigation -->
+        <div class="card card-sidebar-mobile">
+            <ul class="nav nav-sidebar" data-nav-type="accordion">
+
+                <!-- Main -->
+                <li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Main</div> <i class="icon-menu" title="Main"></i></li>
+                <li class="nav-item">
+                    <a href="layout_2/LTR/material/full/index.html" class="nav-link">
+                        <i class="icon-home4"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-item nav-item-submenu nav-item-expanded nav-item-open">
+                    <a href="#" class="nav-link"><i class="icon-stack"></i> <span>Starter kit</span></a>
+
+                    <ul class="nav nav-group-sub" data-submenu-title="Starter kit">
+                        <li class="nav-item"><a href="../seed/sidebar_main.html" class="nav-link active">1 sidebar</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="../full/changelog.html" class="nav-link">
+                        <i class="icon-list-unordered"></i>
+                        <span>Changelog</span>
+                        <span class="badge bg-blue-400 align-self-center ml-auto">2.2</span>
+                    </a>
+                </li>
+                <!-- /main -->
+
+            </ul>
+        </div>
+        <!-- /main navigation -->
+
+    </div>
+    <!-- /sidebar content -->
+
+</div>
